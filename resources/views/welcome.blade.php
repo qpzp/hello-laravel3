8052bdd340c1cbcb71cadec5bd9e74bd7cf3f6da
qpzp <qpzp@hotmail.com>
2019-11-27 13:42:30 +0000

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            width: 100%;
+        }
+
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: sans-serif;
+            color: #777;
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+        }
+
+        .title {
+            font-size: 66px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">yo yo yo - by ftw</div>
+    </div>
+</div>
+</body>
+</html>
